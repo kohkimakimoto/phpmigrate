c5d58cd8ca0ae059318a05365ffbb0127dc341e8
kohkimakimoto <kohki.makimoto@gmail.com>
2013-04-02 14:06:01 +0900

    Accept including other php file.

--- migrate.php
+++ migrate.php
@@ -1,4 +1,3 @@
-#!/usr/bin/env php
 <?php
 /**
  * PHPMigrate
@@ -764,5 +763,8 @@ class MigrationLogger
   }
 }
 
-// Run Command.
-Migration::main();
+if (realpath($_SERVER['SCRIPT_FILENAME']) === __FILE__) {
+  // Run the main program logic, when this script file is directly executed.
+  Migration::main();
+}
+
